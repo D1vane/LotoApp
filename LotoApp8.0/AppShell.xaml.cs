e080b6399707fa92b9ae21f0f6549e07f0ed034a
chayk <chayk@DESKTOP-LN529HM>
2025-01-31 16:11:49 +0700

    Добавьте файлы проекта.

---
+++ LotoApp8.0/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LotoApp8._0
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LotoApp8.0/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LotoApp8._0
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
