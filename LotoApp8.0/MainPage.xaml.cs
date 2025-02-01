70f78a57ab29f08f0d69ba814dc152acb6724b24
Pavel <chayka.pasha32@mail.ru>
2025-02-01 15:32:49 +0700

    добавлены комментарии к коду

--- LotoApp8.0/MainPage.xaml.cs
+++ LotoApp8.0/MainPage.xaml.cs
@@ -8,14 +8,22 @@
             InitializeComponent();
            
         }
-
+        /// <summary>
+        /// Изменение значения слайдера 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
         private void Slider_ValueChanged(object sender, ValueChangedEventArgs args)
         {
             int value = (int)args.NewValue;
             LabelCurrentPosition.Text = Convert.ToString(value);
            
         }
-
+        /// <summary>
+        /// Фиксация нажатия на текущее число
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void LabelCurrentNumber_Clicked(object sender, TappedEventArgs e)
         {
             LabelCurrentNumber.Text = Convert.ToString(Convert.ToInt32(LabelCurrentNumber.Text) + 1);
