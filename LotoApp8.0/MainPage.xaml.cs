7ae5a417fa2cac339039e6ab989cd4939de9621a
Pavel <chayka.pasha32@mail.ru>
2025-02-01 16:23:03 +0700

    добавлена кнопка, которая генерирует случайную последовательность чисел длиной 90 от 1 до 90

--- LotoApp8.0/MainPage.xaml.cs
+++ LotoApp8.0/MainPage.xaml.cs
@@ -2,7 +2,8 @@
 {
     public partial class MainPage : ContentPage
     {
-
+        const int countOfNumbers = 90;
+        int[] numbers = new int[countOfNumbers];
         public MainPage()
         {
             InitializeComponent();
@@ -17,7 +18,9 @@
         {
             int value = (int)args.NewValue;
             LabelCurrentPosition.Text = Convert.ToString(value);
-           
+
+            LabelCurrentNumber.Text = Convert.ToString(numbers[value-1]);
+
         }
         /// <summary>
         /// Фиксация нажатия на текущее число
@@ -27,6 +30,25 @@
         private void LabelCurrentNumber_Clicked(object sender, TappedEventArgs e)
         {
             LabelCurrentNumber.Text = Convert.ToString(Convert.ToInt32(LabelCurrentNumber.Text) + 1);
+        }
+        /// <summary>
+        /// Создание цепочки случайных чисел
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonStart_Clicked(object sender, EventArgs e)
+        {
+            Random r = new();
+            numbers = new int[countOfNumbers];
+            for (int i = 0; i < countOfNumbers; i++)
+            {
+                var tempNum = r.Next(1,countOfNumbers+1);
+                if (numbers.Contains(tempNum)) i--;
+                else numbers[i] = tempNum;
+            }
+            if (numbers.Length != numbers.Distinct().Count()) SliderOfNumbers.Value = 10;
+            else SliderOfNumbers.Value = 1;
+            LabelCurrentNumber.Text = Convert.ToString(numbers[0]);
         }
     }
 
