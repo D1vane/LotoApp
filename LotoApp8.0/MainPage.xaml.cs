05dcb0da814971f979166291a72ed988607280f0
Pavel <chayka.pasha32@mail.ru>
2025-02-01 15:30:20 +0700

    Добавлено изменение порядкого номера текущего числа при движении ползунком. Добавлено событие при нажатии на текущее число для дальнейшего функционала по вводу конкретного числа

--- LotoApp8.0/MainPage.xaml.cs
+++ LotoApp8.0/MainPage.xaml.cs
@@ -6,6 +6,19 @@
         public MainPage()
         {
             InitializeComponent();
+           
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs args)
+        {
+            int value = (int)args.NewValue;
+            LabelCurrentPosition.Text = Convert.ToString(value);
+           
+        }
+
+        private void LabelCurrentNumber_Clicked(object sender, TappedEventArgs e)
+        {
+            LabelCurrentNumber.Text = Convert.ToString(Convert.ToInt32(LabelCurrentNumber.Text) + 1);
         }
     }
 
